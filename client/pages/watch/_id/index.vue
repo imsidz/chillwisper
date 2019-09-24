bb9ad8baa205d77df1f279f0473608424da92f35
imsidz <imsidzluv@gmail.com>
2019-09-24 12:33:22 +0530

    added event

--- client/pages/watch/_id/index.vue
+++ client/pages/watch/_id/index.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-        <youtube :video-id="video.videoId" :player-vars="playerVars" @playing="playing"></youtube>
+        <youtube ref="youtube" :video-id="video.videoId" :player-vars="playerVars" @playing="playing"></youtube>
+        <br>
+        <button class="btn btn-danger" @click="play">Play</button>
+        <button class="btn btn-danger" @click="pause">Pause</button>
+        <br>
+        Check Console for all events
         <p>{{ video.title }}</p>
         <div v-html="video.description">
 
@@ -17,14 +22,30 @@ export default {
     data() {
         return {
             playerVars: {
-                autoplay: 1
+                autoplay: 1,
+                modestbranding: 1,
+                showinfo: 0
             }
         }
     },
     methods: {
         playing() {
             console.log('\o/ we are watching!!!')
+        },
+        play(){
+            this.player.playVideo()
+        },
+        pause(){
+            this.player.pauseVideo()
         }
+    },
+    computed: {
+        player() {
+            return this.$refs.youtube.player
+        }
+    },
+    mounted(){
+        console.log(this.player);
     }
 
 }
